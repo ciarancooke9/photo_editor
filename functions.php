8db3d301c48d51e7b1f77875f56303140b4cb53e
ciarancooke9 <63401191+ciarancooke9@users.noreply.github.com>
2022-02-07 16:22:32 +0000

    page working with code in functions, images still not reshaping properly

--- functions.php
+++ functions.php
@@ -1,4 +1,50 @@
 <?php
+
+
+///this is a function to store a file
+function storeFiles($fileName,$tempFileName, $fileDir){
+    $fileLocation = $fileDir . $fileName;
+
+    move_uploaded_file($tempFileName, $fileLocation);
+
+    return $fileLocation;
+}
+
+///Function to maintain aspect ratio of an image
+function keepAspectRatio($width, $height, $oldWidth, $oldHeight){
+    if (!$width == '')
+    {
+        $factor = (float)$width / (float)$oldWidth;
+        $height = $factor * $oldHeight;
+        return $height;
+    }
+    else if (!$height == '')
+    {
+        $factor = (float)$height / (float)$oldHeight;
+        $width = $factor * $oldWidth;
+        return $width;
+    }
+}
+
+///this function will accept an image file location & parameters to reshape the file (width & height)
+function imageReshaper($image,$height, $width, $keepAspectRatio){
+    ///get height and with Dimensions of original image
+    list($oldheight, $oldwidth) = getimagesize($image);
+    ///check for "keep aspect ratio" feature
+    if ($keepAspectRatio == 'on' && $width != ''){
+        $height = keepAspectRatio($width, $height, $oldwidth, $oldheight);
+    } elseif ($keepAspectRatio == 'on' && $height != ''){
+    $width = keepAspectRatio($width, $height, $oldwidth, $oldheight);
+        }
+
+
+
+    $source = imagecreatefromjpeg($image);
+    $dst = imagecreatetruecolor($width, $height);
+    imagecopyresized($dst, $source, 0, 0, 0, 0, $width, $height, $oldwidth, $oldheight);
+    return $dst;
+}
+
 function photoEdit(){
 if (!$_POST) {
 echo "<img class='img-fluid rounded mb-4 mb-lg-0' src='https://support.apple.com/library/content/dam/edam/applecare/images/en_US/social/supportapphero/camera-modes-hero.jpg' alt='...' />";
@@ -6,15 +52,25 @@ echo "<img class='img-fluid rounded mb-4 mb-lg-0' src='https://support.apple.com
 print_r($_POST);
 $width = $_POST['width'];
 $height = $_POST['height'];
+if (isset($_POST['aspect'])){
+$keepAspectRatio = 'on';
+} else {
+    $keepAspectRatio = 'off';
+}
 $post_image = $_FILES['image']['name'];
 $post_image_temp = $_FILES['image']['tmp_name'];
+
 echo "<h1>$post_image</h1>";
 echo "<h2>Height:$height</h2>". ' ' . "<h2>Width:$width</h2>";
 
-move_uploaded_file($post_image_temp, "images/$post_image");
-$original = "images/".$_FILES['image']['name'];
 
-list($oldheight, $oldwidth) = getimagesize($original);
+$original = storeFiles($post_image,$post_image_temp, 'images/');
+echo $original;
+
+$reshapedImage = imageReshaper($original, $height, $width, $keepAspectRatio);
+$thumb = 'images/blabla' . $_FILES['image']['name'];
+imagejpeg($reshapedImage, $thumb, 100);
+/*list($oldheight, $oldwidth) = getimagesize($original);
 
 if (isset($_POST['aspect'])){
 if (!$width == '')
@@ -33,9 +89,10 @@ $newfile = imagecreatefromjpeg($original);
 $thumb = 'images/blabla' . $_FILES['image']['name'];
 $resized = imagecreatetruecolor($width, $height);
 imagecopyresampled($resized, $newfile, 0, 0, 0, 0, $width, $height, $oldwidth, $oldheight);
-imagejpeg($resized, $thumb, 100);
+imagejpeg($resized, $thumb, 100);*/
+
 unlink($original);
-echo "<img class='img-fluid rounded mb-4 mb-lg-0'  src='$thumb' height='$height' width='$width' />";
+echo "<img class='img-fluid rounded mb-4 mb-lg-0'  src='$thumb' height='$height' width='$width'/>";
 ///unlink($thumb);
 }
 }
